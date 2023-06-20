6b5cccf969d894df6ff74ba277986f2f76e3db6b
Leenday <iammilagre@gmail.com>
2023-06-20 22:34:15 +0600

    add solution to maps exercises

--- maps/maps.py
+++ maps/maps.py
@@ -14,6 +14,30 @@ class MapExercise:
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
         pass
+        ratings = list(map(MapExercise.get_ratings, list_of_movies))
+        ratings_sum, movies_count = MapExercise.calculate_ratings(ratings)
+        return ratings_sum / movies_count
+
+    @staticmethod
+    def get_ratings(movie: dict) -> list:
+        if not movie["rating_kinopoisk"]:
+            return 0
+        if len(movie["country"].split(",")) >= 2 and float(movie["rating_kinopoisk"]) > 0:
+            return float(movie["rating_kinopoisk"])
+        else:
+            return 0
+
+    @staticmethod
+    def calculate_ratings(ratings: list[float]) -> tuple:
+        movies_count = 0
+        rating_sum = 0
+        for i in ratings:
+            if i != 0:
+                movies_count += 1
+                rating_sum += i
+            else:
+                continue
+        return (rating_sum, movies_count)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -29,3 +53,20 @@ class MapExercise:
         или равным заданному значению
         """
         pass
+        a = list(map(lambda mov: MapExercise.get_movies(mov, rating), list_of_movies))
+        result = 0
+        for i in a:
+            if not i:
+                continue
+            else:
+                result += i.count("и")
+        return result
+
+    @staticmethod
+    def get_movies(movie: dict, rating: Union[float, int]) -> list:
+        if not movie["rating_kinopoisk"]:
+            return False
+        if float(movie["rating_kinopoisk"]) >= rating:
+            return movie["name"]
+        else:
+            return False
