defafabbfd2386f07a82227a85738d54669c0ba6
Leenday <iammilagre@gmail.com>
2023-06-21 17:55:16 +0600

    add solution for flask exercise

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 class FlaskExercise:
@@ -26,6 +26,41 @@ class FlaskExercise:
     В ответ должен вернуться статус 204
     """
 
+    db = {}
+
     @staticmethod
     def configure_routes(app: Flask) -> None:
         pass
+
+        @app.post("/user")
+        def create_user():
+            try:
+                name = request.get_json()["name"]
+            except KeyError:
+                response = {"errors": {"name": "This field is required"}}
+                return response, 422
+            else:
+                FlaskExercise.db[name] = "created"
+                response = {"data": f"User {name} is created!"}
+                return response, 201
+
+        @app.get("/user/<username>")
+        def get_user(username):
+            if FlaskExercise.db[username] == "deleted":
+                return "", 404
+            response = {"data": f"My name is {username}"}
+            return response, 200
+
+        @app.patch("/user/<username>")
+        def update_user(username):
+            name = request.get_json()["name"]
+            FlaskExercise.db[username] = "deleted"
+            FlaskExercise.db[name] = "created"
+            response = {"data": f"My name is {name}"}
+            return response, 200
+
+        @app.delete("/user/<username>")
+        def delete_user(username):
+            FlaskExercise.db[username] = "deleted"
+            response = {"data": f"{username} deleted"}
+            return "", 204
